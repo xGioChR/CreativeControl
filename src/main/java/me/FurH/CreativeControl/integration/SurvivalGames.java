57c9bc0c35cf98e0937ad0fd3d5b61005aec9a86
Luis <lgpserranegra@hotmail.com>
2013-03-18 13:09:47 -0300

    Change lisence

--- src/main/java/me/FurH/CreativeControl/integration/SurvivalGames.java
+++ src/main/java/me/FurH/CreativeControl/integration/SurvivalGames.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011-2012 FurmigaHumana.  All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation,  version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.FurH.CreativeControl.integration;
 
 import me.FurH.CreativeControl.CreativeControl;
@@ -11,7 +27,6 @@ import org.mcsg.survivalgames.api.PlayerJoinArenaEvent;
 /**
  *
  * @author FurmigaHumana
- * All Rights Reserved unless otherwise explicitly stated.
  */
 public class SurvivalGames implements Listener {
 
