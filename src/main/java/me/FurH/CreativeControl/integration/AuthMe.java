7f7427ea26fcbe356bd6eab13693d36eb90b4f5e
Luis <lgpserranegra@hotmail.com>
2013-05-09 03:14:04 -0300

    Trying AuthMe compatibility [2]

--- src/main/java/me/FurH/CreativeControl/integration/AuthMe.java
+++ src/main/java/me/FurH/CreativeControl/integration/AuthMe.java
@@ -2,6 +2,7 @@ package me.FurH.CreativeControl.integration;
 
 import me.FurH.CreativeControl.CreativeControl;
 import me.FurH.CreativeControl.data.CreativePlayerData;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -20,18 +21,36 @@ import uk.org.whoami.authme.events.StoreInventoryEvent;
 public class AuthMe implements Listener {
     
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
+    public void onInventoryStore(StoreInventoryEvent e) {
+        if (e.isCancelled()) { return; }
+
+        CreativeControl       plugin      = CreativeControl.getPlugin();
+        CreativePlayerData    data        = CreativeControl.getPlayerData();
+        final Player p = e.getPlayer();
+
+        data.save(p, p.getGameMode());
+        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+            @Override
+            public void run() {
+                onInventoryReset(p);
+            }
+        }, 3L);
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
     public void onInventoryRestore(RestoreInventoryEvent e) {
         if (e.isCancelled()) { return; }
 
-        CreativePlayerData    data        = CreativeControl.getPlayerData();
-        Player p = e.getPlayer();
+        CreativeControl       plugin      = CreativeControl.getPlugin();
+        final CreativePlayerData    data        = CreativeControl.getPlayerData();
+        final Player p = e.getPlayer();
 
-        System.out.println("RESTORE: " + p.getGameMode().name());
-        
-        data.save(p, p.getGameMode());
-        onInventoryReset(p);
-        
-        e.setCancelled(true);
+        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+            @Override
+            public void run() {
+                data.restore(p, p.getGameMode());
+            }
+        }, 2L);
     }
     
     public void onInventoryReset(Player p) {
@@ -39,19 +58,5 @@ public class AuthMe implements Listener {
         p.getInventory().setArmorContents(new ItemStack[] { new ItemStack(Material.AIR, 1)});
         p.getInventory().setContents(new ItemStack[] { new ItemStack(Material.AIR, 1)});
         
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
-    public void onInventoryStore(StoreInventoryEvent e) {
-        if (e.isCancelled()) { return; }
-        
-        CreativePlayerData    data        = CreativeControl.getPlayerData();
-        Player p = e.getPlayer();
-        
-        System.out.println("STORE: " + p.getGameMode().name());
-        
-        data.restore(p, p.getGameMode());
-        
-        e.setCancelled(true);
     }
 }
