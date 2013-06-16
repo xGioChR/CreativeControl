8605bc60744fd97624c93eea55f7161a98d6c684
Luis <lgpserranegra@hotmail.com>
2013-06-16 06:06:05 -0300

    Just some formatting

--- src/main/java/me/FurH/CreativeControl/listener/CreativeBlockListener.java
+++ src/main/java/me/FurH/CreativeControl/listener/CreativeBlockListener.java
@@ -63,8 +63,11 @@ public class CreativeBlockListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent e) {
         if (e.isCancelled()) { return; }
-        if (e.getPlayer() == null) { return; }
-        
+
+        if (e.getPlayer() == null) {
+            return;
+        }
+
         Player p = e.getPlayer();
         Block b = e.getBlockPlaced();
         World world = p.getWorld();
@@ -238,8 +241,11 @@ public class CreativeBlockListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent e) {
         if (e.isCancelled()) { return; }
-        if (e.getPlayer() == null) { return; }
-        
+
+        if (e.getPlayer() == null) {
+            return;
+        }
+
         Player p = e.getPlayer();
         Block b = e.getBlock();
         World world = b.getWorld();
